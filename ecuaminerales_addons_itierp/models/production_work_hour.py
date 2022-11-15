3cff887de5e2220cb9abeb5330e2c35f736fd2cc
Ricardo Vinicio Jara Jara <79996025+rvjjcolina@users.noreply.github.com>
2022-11-14 22:50:49 -0500

    [ADD] Suma de extraordinaras

--- ecuaminerales_addons_itierp/models/production_work_hour.py
+++ ecuaminerales_addons_itierp/models/production_work_hour.py
@@ -47,13 +47,17 @@ class ProductionWorkHour(models.Model):
     festivo_end = fields.Datetime('Fin Feriado')
 
     @api.multi
+    def delete_dias_festivos(self):
+        self.hour_production_ids.write({'festivo': False})
+
+    @api.multi
     def insert_dias_festivos(self):
         for employee_id in set(self.hour_production_ids.mapped('employee_id')):
             list_hours = self.hour_production_ids.filtered(lambda x: x.employee_id == employee_id).sorted('fecha_time')
             count = 1
             for ahora in list_hours[1:]:
                 antes = list_hours[count - 1]
-                if ahora.fecha_time >= self.festivo_end and antes.fecha_time >= self.festivo_start:
+                if ahora.fecha_time <= self.festivo_end and antes.fecha_time >= self.festivo_start:
                     antes.festivo = True
                     ahora.festivo = True
 
